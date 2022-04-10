87e650cd7fe17e6902f566d3bd71a197686e10e4
hrflkner <hunter.wm.faulkner@gmail.com>
2022-04-10 14:45:43 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/FirstNonConsecutive.js
@@ -0,0 +1,12 @@
+// Solution : Looping
+
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] + 1 !== arr[i + 1]) {
+            return arr[i + 1];
+        }
+    }
+    return null;
+}
+
+console.log(firstNonConsecutive([1, 2, 4, 5]));
