97b3e127ea51b1b6c000908cf8c65324fd0636ab
hrflkner <hunter.wm.faulkner@gmail.com>
2022-04-20 00:29:49 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/NameShuffler.js
@@ -0,0 +1,7 @@
+// Solution : String to Array, Reverse Array, Array to String
+
+function nameShuffler(str) {
+    return str.split(' ').reverse().join(' ');
+}
+
+console.log(nameShuffler('faulkner hunter'));
