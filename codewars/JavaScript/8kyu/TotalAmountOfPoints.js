f02f78497ea18539db33c136ef0ca83f98330865
hrflkner <hunter.wm.faulkner@gmail.com>
2022-04-27 22:16:06 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/TotalAmountOfPoints.js
@@ -0,0 +1,15 @@
+// Solution
+
+function points(games) {
+    let res = 0;
+    for (let i = 0; i < games.length; i++) {
+        if (games[i][0] > games[i][2]) {
+            res += 3;
+        } else if (games[i][0] == games[i][2]) {
+            res += 1;
+        }
+    }
+    return res;
+}
+
+console.log(points(['1:2', '3:0', '1:1']));
