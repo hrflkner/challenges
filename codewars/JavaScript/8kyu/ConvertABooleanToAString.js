211d4a37e43e52eb02871381ba3566abdff18f41
hrflkner <hunter.wm.faulkner@gmail.com>
2022-04-27 19:40:01 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/ConvertABooleanToAString.js
@@ -0,0 +1,11 @@
+// Solution 1: ternary
+
+function booleanToString(b) {
+    return b ? 'true' : 'false';
+}
+
+// Solution 2 : .toString() - Readable
+
+function booleanToString(b) {
+    return b.toString();
+}
