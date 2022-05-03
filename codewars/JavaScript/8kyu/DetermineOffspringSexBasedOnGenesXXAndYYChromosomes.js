90ffa6ac24e86329a31c929abec61a6fe8479611
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-03 18:27:21 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/DetermineOffspringSexBasedOnGenesXXAndYYChromosomes.js
@@ -0,0 +1,8 @@
+function chromosomeCheck(sperm) {
+    return (
+        "Congratulations! You're going to have a " +
+        (sperm.includes('Y') ? 'son.' : 'daughter.')
+    );
+}
+
+console.log(chromosomeCheck('XY'));
