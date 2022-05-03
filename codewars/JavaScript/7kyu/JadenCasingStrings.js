5a8fe6c78b407dbe29e9a03a32741a14f55e1e48
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-03 23:21:16 +1000

    Add Challenge

---
+++ codewars/JavaScript/7kyu/JadenCasingStrings.js
@@ -0,0 +1,7 @@
+// Solution : Map to title case every word
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ')
+        .map((x) => x[0].toUpperCase() + x.slice(1))
+        .join(' ');
+};
