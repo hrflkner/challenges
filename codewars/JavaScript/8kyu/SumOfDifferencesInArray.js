485a09434b87d7921fbae0735d646dcdfbeeff34
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-11 17:45:29 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/SumOfDifferencesInArray.js
@@ -0,0 +1,11 @@
+function sumOfDifferences(arr) {
+    arr.sort((a, b) => b - a);
+    let res = 0;
+
+    for (let i = 0; i < arr.length - 1; ++i) {
+        res += arr[i] - arr[i + 1];
+    }
+    return res;
+}
+
+console.log(sumOfDifferences([10, 2, 1]));
