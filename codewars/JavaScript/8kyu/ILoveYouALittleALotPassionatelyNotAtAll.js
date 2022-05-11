5244c988f7f2a90d972321d6daea7daa1b84fc0a
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-11 17:29:33 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/ILoveYouALittleALotPassionatelyNotAtAll.js
@@ -0,0 +1,12 @@
+function howMuchILoveYou(nbPetals) {
+    return [
+        'I love you',
+        'a little',
+        'a lot',
+        'passionately',
+        'madly',
+        'not at all',
+    ][nbPetals % 6 !== 0 ? (nbPetals % 6) - 1 : 5];
+}
+
+console.log(howMuchILoveYou(6));
