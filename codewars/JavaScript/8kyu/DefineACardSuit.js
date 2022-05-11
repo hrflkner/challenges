c2ff521896a053aa9174f60a1a2ccf36ea603cb4
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-11 16:31:02 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/DefineACardSuit.js
@@ -0,0 +1,13 @@
+function defineSuit(card) {
+    if (card.substr(card.length - 1) === '♣') {
+        return 'clubs';
+    } else if (card.substr(card.length - 1) === '♦') {
+        return 'diamonds';
+    } else if (card.substr(card.length - 1) === '♥') {
+        return 'hearts';
+    } else if (card.substr(card.length - 1) === '♠') {
+        return 'spades';
+    }
+}
+
+console.log(defineSuit('Q♠'));
