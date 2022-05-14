57f3d8c887079bfa7dd5f84c3af3f92b131005fe
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-14 12:48:56 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/CSVRepresentationOfArray.js
@@ -0,0 +1,32 @@
+// Solution : Without .join
+
+function toCsvText(array) {
+    let res = '';
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (i === array.length - 1 && j === array[i].length - 1) {
+                res += `${array[i][j]}`;
+            } else if (j === array[i].length - 1) {
+                res += `${array[i][j]}\n`;
+            } else {
+                res += `${array[i][j]},`;
+            }
+        }
+    }
+    return res;
+}
+
+// Solution : With .join
+
+function toCsvText(array) {
+    return array.join('\n');
+}
+
+console.log(
+    toCsvText([
+        [0, 1, 2, 3, 45],
+        [10, 11, 12, 13, 14],
+        [20, 21, 22, 23, 24],
+        [30, 31, 32, 33, 34],
+    ])
+);
