d1bc886a7c9504d96d5ad39b428179f78444bcf2
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-16 17:29:35 +1000

    Add Challenge

---
+++ codewars/JavaScript/8kyu/FindMaximumAndMinimumValuesOfAList.js
@@ -0,0 +1,9 @@
+// Solution : .min(... arr ) Note: Must use '...' to unpack array elements
+
+var max = function (list) {
+    return Math.max(...list);
+};
+
+var min = function (list) {
+    return Math.min(...list);
+};
