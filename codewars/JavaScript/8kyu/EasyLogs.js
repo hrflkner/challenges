f4a12149d574c2756fe274206a90cf4e60c79a59
hrflkner <hunter.wm.faulkner@gmail.com>
2022-06-08 16:25:18 +1000

    Add Challenge

--- codewars/JavaScript/8kyu/EasyLogs.js
+++ codewars/JavaScript/8kyu/EasyLogs.js
@@ -3,3 +3,7 @@
 function logs(x, a, b) {
     return Math.log(a) / Math.log(x) + Math.log(b) / Math.log(x);
 }
+
+// Solution 2 : Simplified Log Equation
+
+const logs = (x, a, b) => Math.log(a * b) / Math.log(x);
