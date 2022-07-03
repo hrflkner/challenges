17d93823e3798c66c1d64ff240aaa8c4e6bb9a56
hrflkner <hunter.wm.faulkner@gmail.com>
2022-07-03 16:43:21 +1000

    Add Challenge

--- leetcode/Medium/AddTwoNumbers.js
+++ leetcode/Medium/AddTwoNumbers.js
@@ -1,24 +1,29 @@
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
 var addTwoNumbers = function (l1, l2) {
-    let int1 = parseInt(
-        l1
-            .map((x) => x.toString())
-            .reverse()
-            .join('')
-    );
-    let int2 = parseInt(
-        l2
-            .map((x) => x.toString())
-            .reverse()
-            .join('')
-    );
+    let head = new ListNode();
+    let answerPointer = head;
 
-    let sol = (int1 + int2)
-        .toString()
-        .split('')
-        .map((x) => parseInt(x))
-        .reverse();
+    let carryTheOne = 0;
 
-    return sol;
+    while (l1 || l2 || carryTheOne) {
+        let sum = (l1?.val || 0) + (l2?.val || 0) + carryTheOne;
+        answerPointer.next = new ListNode(sum % 10);
+        answerPointer = answerPointer.next;
+
+        carryTheOne = Math.floor(sum / 10);
+
+        l1 = l1?.next;
+        l2 = l2?.next;
+    }
+
+    return head.next;
 };
 
-console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
+let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+console.log(addTwoNumbers(l1, l2));
